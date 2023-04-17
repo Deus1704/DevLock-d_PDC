ba271b7f1949cbadca1629895cf186eadc03a4a0
Jay Dulange <117574289+Deus1704@users.noreply.github.com>
2023-04-17 15:02:45 +0530

    Added files

---
+++ Teams.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:4,
+        autoplay:false,
+        margin:30,
+        loop:true,
+        nav:true,
+        navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>" ]
+    });
+  });
